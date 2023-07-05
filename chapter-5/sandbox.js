82772fa480779c986be591d80144790642472c6c
orewa-arun <arunworkacc0@gmail.com>
2023-07-05 11:25:44 +0530

    chapter 5

---
+++ chapter-5/sandbox.js
@@ -0,0 +1,9 @@
+const age = 21;
+
+// Note that == loose comparison doesn't care about type compared
+console.log(age == "21");
+
+// === strict comparison cares about type
+console.log(age === "21");
+console.log(age !== 21);
+console.log(age === 21);
