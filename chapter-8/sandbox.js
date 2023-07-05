276029eb602acbb0bc2d281b5cfc702e6429c399
orewa-arun <arunworkacc0@gmail.com>
2023-07-05 18:08:44 +0530

    chapter-8

---
+++ chapter-8/sandbox.js
@@ -0,0 +1,15 @@
+let age = 30;
+
+if (age >= 30) {
+    let age = 22;
+    console.log(age);
+
+    if(age === 22){
+        age = 21;
+        console.log(age);
+    }
+
+    console.log(age);
+}
+
+console.log(age);
